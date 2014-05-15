9c808ff225823112a41f645629f87846e945ce4c
Nils Grunwald <nils@grunwald.fr>
2014-05-15 19:15:51 +0200

    update dependencies

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject ultra-csv "0.1.1-SNAPSHOT"
-  :description "A smart reader/writer for CSV files"
+(defproject ultra-csv "0.2.0-SNAPSHOT"
+  :description "A smart reader for CSV files"
   :url "https://github.com/ngrunwald/ultra-csv"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [net.sf.supercsv/super-csv "2.1.0"]
-                 [com.ibm.icu/icu4j "52.1"]
+                 [net.sf.supercsv/super-csv "2.2.0"]
+                 [com.ibm.icu/icu4j "53.1"]
                  [commons-io "2.4"]])
